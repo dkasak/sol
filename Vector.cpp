2e3b2dcad5e2cd04345de3abef31c9b3eb5bbf4f
Denis Kasak <denis.kasak@gmail.com>
2010-02-24 00:20:45 +0100

    Added Vector class.

---
+++ Vector.cpp
@@ -0,0 +1,161 @@
+#include <cmath>
+#include <ostream>
+
+#include "vector.h"
+
+namespace Sol {
+
+Vector::Vector() {
+    this->x = 0;
+    this->y = 0;
+    this->z = 0;
+}
+
+Vector::Vector(double x, double y, double z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+double Vector::getX() const {
+    return this->x;
+}
+
+double Vector::getY() const {
+    return this->y;
+}
+
+double Vector::getZ() const {
+    return this->z;
+}
+
+void Vector::setX(double x) {
+    this->x = x;
+}
+
+void Vector::setY(double y) {
+    this->y = y;
+}
+
+void Vector::setZ(double Z) {
+    this->z = z;
+}
+
+Vector Vector::operator+(const Vector &v) const {
+    double i, j, k;
+
+    i = this->x + v.x;
+    j = this->y + v.y;
+    k = this->z + v.z;
+
+    return Vector(i, j, k);
+}
+
+Vector Vector::operator*(const double r) const {
+    double i, j, k;
+
+    i = this->x * r;
+    j = this->y * r;
+    k = this->z * r;
+
+    return Vector(i, j, k);
+}
+
+Vector Vector::operator/(const double r) const {
+    double i, j, k;
+
+    i = this->x / r;
+    j = this->y / r;
+    k = this->z / r;
+
+    return Vector(i, j, k);
+}
+
+double Vector::dot(const Vector &v) const {
+    return this->x * v.x +
+           this->y * v.y +
+           this->z * v.z;
+}
+
+Vector Vector::cross(const Vector &v) const {
+    double i, j, k;
+
+    i = this->y * v.z - this->z * v.y;
+    j = this->z * v.x - this->x * v.z;
+    k = this->x * v.y - this->y * v.x;
+
+    return Vector(i, j, k);
+}
+
+Vector& Vector::operator+=(const Vector &v) {
+    this->x += v.x;
+    this->y += v.y;
+    this->z += v.z;
+
+    return *this;
+}
+
+Vector& Vector::operator*=(const double r) {
+    this->x *= r;
+    this->y *= r;
+    this->z *= r;
+
+    return *this;
+}
+
+Vector& Vector::operator/=(const double r) {
+    this->x /= r;
+    this->y /= r;
+    this->z /= r;
+
+    return *this;
+}
+
+Vector& Vector::operator=(const Vector &v) {
+    this->x = v.x;
+    this->y = v.y;
+    this->z = v.z;
+
+    return *this;
+}
+
+double Vector::angle(const Vector &v) const {
+    double cosine = this->dot(v) / (this->length() * v.length());
+
+    if (cosine > 1.0) {
+        cosine = 1.0;
+    } else if (cosine < -1.0) {
+        cosine = -1.0;
+    }
+
+    return acos(cosine);
+}
+
+double Vector::length() const {
+    return sqrt(pow(this->x, 2.0) + pow(this->y, 2.0) + pow(this->z, 2.0));
+}
+
+Vector Vector::normalised() const {
+    Vector tmp = (*this);
+
+    tmp /= tmp.length();
+
+    return tmp;
+}
+
+void Vector::normalise() {
+    (*this) /= this->length();
+}
+
+} // namespace Sol
+
+std::ostream& operator<<(std::ostream& o, const Sol::Vector &v) {
+    o << v.getX();
+    o << "i + ";
+    o << v.getY();
+    o << "j + ";
+    o << v.getZ();
+    o << "k";
+
+    return o;
+}
---
+++ Vector.h
@@ -0,0 +1,47 @@
+#include <iostream>
+
+#ifndef SOL_VECTOR_H
+#define SOL_VECTOR_H
+
+namespace Sol {
+
+class Vector {
+
+    private:
+        double x, y, z;
+
+    public:
+        Vector();
+        Vector(double x, double y, double z);
+
+        double getX() const;
+        double getY() const;
+        double getZ() const;
+        void setX(double x);
+        void setY(double y);
+        void setZ(double z);
+
+        Vector operator+(const Vector &v) const;
+        Vector operator*(const double r) const;
+        Vector operator/(const double r) const;
+        double dot(const Vector &v) const;
+        Vector cross(const Vector &v) const;
+        double angle(const Vector &v) const;
+
+        Vector& operator+=(const Vector &v);
+        Vector& operator*=(const double r);
+        Vector& operator/=(const double r);
+
+        Vector& operator=(const Vector &v);
+
+        void normalise();
+        Vector normalised() const;
+
+        double length() const;
+};
+
+} // namespace Sol
+
+std::ostream& operator<<(std::ostream& o, const Sol::Vector &v);
+
+#endif // SOL_VECTOR_H
