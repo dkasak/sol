a7eae0b89f2f5054e1a8173dd968d6a274459553
Denis Kasak <denis.kasak@gmail.com>
2012-02-18 20:14:58 +0100

    Get rid of Scene::eye, we already have Scene::origin.

--- Scene.h
+++ Scene.h
@@ -29,7 +29,6 @@ class Scene {
 
 private:
     Point  origin;
-    Point  eye;
     Screen screen;
     vector<const Shape&> objects;
     vector<const Light&> lights;
@@ -39,7 +38,6 @@ public:
     Scene();
     void setOrigin(const Point &p);
     void setScreen(const Screen &s);
-    void setEye(const Point &e);
     void addShape(const Shape &s);
     void addLight(const Light &l);
     void setAmbient(double ambient);
