94c9d0e1c1956aec5f32185dc19ca0cc6257f2b7
Denis Kasak <denis.kasak@gmail.com>
2013-05-12 22:36:39 +0200

    Implement a basic debug output interface.

---
+++ Debug.cpp
@@ -0,0 +1,3 @@
+#include "Debug.h"
+
+int debug_level;
---
+++ Debug.h
@@ -0,0 +1,42 @@
+/**
+ * Copyright 2011, 2012 Denis Kasak <denis.kasak@gmail.com>
+ * 
+ * This file is part of Sol.
+ * 
+ * Sol is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Sol is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Sol. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SOL_DEBUG_H
+#define SOL_DEBUG_H
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+template <typename T>
+void DEBUG(int n, T out) {
+    if (DEBUG_LEVEL >= n) {
+        cout << out << endl;
+    }
+}
+
+template <typename T, typename Q>
+void DEBUG(int n, T out1, Q out2) {
+    if (DEBUG_LEVEL >= n) {
+        cout << out1 << " " << out2 << endl;
+    }
+}
+
+#endif // SOL_DEBUG_H
+
