d239c4bc6f1a8ff0b3230d8eb64a83e2515f170e
Denis Kasak <denis.kasak@gmail.com>
2013-08-30 21:19:40 +0200

    Use system_clock when calculating estimate.

--- Debug.cpp
+++ Debug.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 using std::chrono::steady_clock;
+using std::chrono::system_clock;
 
 int debug_level;
 
@@ -15,7 +16,7 @@ print_progress(steady_clock::time_point start, size_t complete,
     auto elapsed_seconds = elapsed % 60;
     double percent = double(current) / complete;
     auto estimate = int(elapsed * 1/percent - elapsed);
-    std::time_t eta = steady_clock::to_time_t(steady_clock::now()) + estimate;
+    std::time_t eta = system_clock::to_time_t(system_clock::now()) + estimate;
     auto minutes_left = estimate / 60;
     auto seconds_left = estimate % 60;
     stringstream ss;
